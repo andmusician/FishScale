756218c4c3cee35b0b112428a91ff6a670022036
Anderson de Oliveira Lima <anderson.nlima@tivit.com>
2018-03-30 18:16:07 -0300

    Criação da Model Cliente (Classe Cliente)

---
+++ FishScale/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace FishScale.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public int ClienteId { get; set; }
+        public int MaquinaID { get; set; }
+        public string SistemaOperacional { get; set; }
+        public double FreeHD { get; set; }
+        public double UsedHD { get; set; }
+        public double FreeRam { get; set; }
+        public double UsedRam { get; set; }
+
+    }
+}
