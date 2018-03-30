4682c93471890e76dbfd1a0f609d1d89dacce271
Anderson de Oliveira Lima <anderson.nlima@tivit.com>
2018-03-30 19:59:14 -0300

    Criação Model(Cliente), Controller(Cliente), view (Cliente)

---
+++ FishScale/Controllers/ClienteController.cs
@@ -0,0 +1,38 @@
+﻿using FishScale.Models;
+using System.Web.Mvc;
+
+
+namespace FishScale.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public ActionResult Cliente() 
+        {
+            // Criando um objeto do tipo Cliente da Model
+            // IMPORTANTE: OS VALORES DAS VARIÁVEIS SERÃO CHAMADOS DO SQL SERVER
+
+            var cliente = new Cliente 
+            {
+                Nome = "BandTec", 
+                ClienteId = 1, 
+                MaquinaId = 1, 
+                SistemaOperacional = "Windows", 
+                FreeHD = 100, 
+                UsedHD = 50, 
+                FreeRam = 4000, 
+                UsedRam = 2000 
+            };
+            ViewData["Nome"] = cliente.Nome;
+            ViewData["ClienteId"] = cliente.ClienteId;
+            ViewData["MaquinaId"] = cliente.MaquinaId;
+            ViewData["SistemaOperacional"] = cliente.SistemaOperacional;
+            ViewData["FreeHD"] = cliente.FreeHD;
+            ViewData["UsedHD"] = cliente.UsedHD;
+            ViewData["FreeRam"] = cliente.FreeRam;
+            ViewData["UsedRam"] = cliente.UsedRam;
+            
+
+            return View(); // Para Criar uma VIEW Empty(whitout Model) e deixar selecionado Use a Layout Page
+        }
+    }
+}
--- FishScale/Models/Cliente.cs
+++ FishScale/Models/Cliente.cs
@@ -4,12 +4,13 @@
     {
         public string Nome { get; set; }
         public int ClienteId { get; set; }
-        public int MaquinaID { get; set; }
+        public int MaquinaId { get; set; }
         public string SistemaOperacional { get; set; }
         public double FreeHD { get; set; }
         public double UsedHD { get; set; }
         public double FreeRam { get; set; }
         public double UsedRam { get; set; }
 
+        // VIDEO ASP.Net MVC na Prática - Parte 2 - TEMPO: 15:22
     }
 }
