b953561841b279c0f74a73e94471f3e82441a651
Anderson de Oliveira Lima <anderson.nlima@tivit.com>
2018-04-02 19:23:42 -0300

    Instalação Angular e Entity Framework

--- FishScale/Controllers/ClienteController.cs
+++ FishScale/Controllers/ClienteController.cs
@@ -7,36 +7,36 @@ namespace FishScale.Controllers
 {
     public class ClienteController : Controller
     {
-        public ActionResult Cliente() 
+        public ActionResult Cliente()
         {
             // Criando um objeto do tipo Cliente da Model
             // IMPORTANTE: OS VALORES DAS VARIÁVEIS SERÃO CHAMADOS DO SQL SERVER
 
-            SqlConnection minhaconexao = new SqlConnection("Server=tcp:domotichouse.database.windows.net,1433; Database=fishscale; Initial Catalog=Domotic;Persist Security Info=False;User ID=andmusician;Password=mficbr100%;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            ////SqlConnection minhaconexao = new SqlConnection("Server=tcp:domotichouse.database.windows.net,1433;Database=Domotic;User ID=andmusician@domotichouse;Password=mficbr100%;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            //SqlConnection minhaconexao = new SqlConnection("Server=tcp:domotichouse.database.windows.net,1433;Initial Catalog=Domotic;Persist Security Info=False;User ID=andmusician;Password=mficbr100%;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
-            minhaconexao.Open();
+            //minhaconexao.Open();
 
-            string strQuerySelect = "SELECT * FROM FISHSCALE";
-            SqlCommand cmdComandoSelect = new SqlCommand(strQuerySelect, minhaconexao);
-            SqlDataReader dados = cmdComandoSelect.ExecuteReader();
+            //string strQuerySelect = "SELECT * FROM FISHSCALE";
+            //SqlCommand cmdComandoSelect = new SqlCommand(strQuerySelect, minhaconexao);
+            //SqlDataReader dados = cmdComandoSelect.ExecuteReader();
 
-            while (dados.Read())
+            //while (dados.Read())
+            //{
+            //    System.Console.WriteLine($"clienteid:{0}, nome:{1}, maquinaid:{2}, sistemaoperacional:{3}, freehd: {4}, usedhd: {5}, freeram: {6}, usedram: {7}, dataview: {8}", dados["clienteid"], dados["nome"], dados["maquinaid"], dados["sistemaoperacional"], dados["freehd"], dados["usedhd"], dados["freeram"], dados["usedram"], dados["dataview"]);
+            //}
+
+
+            var cliente = new Cliente
             {
-                System.Console.WriteLine($"clienteid:{0}, nome:{1}, maquinaid:{2}, sistemaoperacional:{3}, freehd: {4}, usedhd: {5}, freeram: {6}, usedram: {7}, dataview: {8}", dados["clienteid"], dados["nome"], dados["maquinaid"], dados["sistemaoperacional"], dados["freehd"], dados["usedhd"], dados["freeram"], dados["usedram"], dados["dataview"]);
-            }
-            
-
-
-            var cliente = new Cliente 
-            {
-                Nome = "Bandtec", 
-                ClienteId = 1, 
-                MaquinaId = 1, 
-                SistemaOperacional = "Windows", 
-                FreeHD = 100, 
-                UsedHD = 50, 
-                FreeRam = 4000, 
-                UsedRam = 2000 
+                Nome = "Bandtec",
+                ClienteId = 1,
+                MaquinaId = 1,
+                SistemaOperacional = "Windows",
+                FreeHD = 100,
+                UsedHD = 50,
+                FreeRam = 4000,
+                UsedRam = 2000
             };
             ViewBag.Nome = cliente.Nome;
             ViewBag.ClienteId = cliente.ClienteId;
@@ -46,18 +46,11 @@ namespace FishScale.Controllers
             ViewBag.UsedHD = cliente.UsedHD;
             ViewBag.FreeRam = cliente.FreeRam;
             ViewBag.UsedRam = cliente.UsedRam;
-            
+
 
             return View(); // Para Criar uma VIEW Empty(whitout Model) e deixar selecionado Use a Layout Page
         }
-        
-        //Definir um método que busca o cliente pelo Nome e retorna seus dados na tela
-        //[HttpPost]
-        //public ActionResult NomeCliente(string NomeCliente)
-        //{
-        //    ViewBag.Nome = NomeCliente;
 
-        //    return View();
-        //}
+        
     }
 }
