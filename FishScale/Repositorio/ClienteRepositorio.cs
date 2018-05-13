e239d3a7582602b7c504ccbc21d5b8f43102ad0b
Anderson de Oliveira Lima <anderson.nlima@tivit.com>
2018-05-12 21:32:31 -0300

    Criando a pasta de repositório que será responsável pelo CRUD

---
+++ FishScale/Repositorio/ClienteRepositorio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using FishScale.Models;
+
+
+namespace FishScale.Repositorio
+{
+    public class ClienteRepositorio
+    {
+        private SqlConnection conn1;
+
+        private void Connection()
+        {
+            string construtor = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+            conn1 = new SqlConnection();
+        }
+
+        public bool AdicionarCliente(Cliente clienteObj)
+        {
+                
+            Connection();
+
+            int i;
+
+            using (SqlCommand command = new SqlCommand("IncluirCliente", conn1))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@QtdeMaquinas", clienteObj.QtdeMaquinas);
+                command.Parameters.AddWithValue("@NomeCliente", clienteObj.NomeCliente);
+                command.Parameters.AddWithValue("@AlertaHD", clienteObj.AlertaHD);
+                command.Parameters.AddWithValue("@AlertaRAM", clienteObj.AlertaRam);
+
+                conn1.Open();
+
+                i = command.ExecuteNonQuery();
+
+            }
+
+            conn1.Close();
+
+            return i >= 1;
+        }
+    }
+}
