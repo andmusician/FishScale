c45f9d7e19df2876a979eabc1cf342472ecf1203
Anderson de Oliveira Lima <anderson.nlima@tivit.com>
2018-05-12 23:05:14 -0300

    Criando o RUD do repositório Cliente

--- FishScale/Repositorio/ClienteRepositorio.cs
+++ FishScale/Repositorio/ClienteRepositorio.cs
@@ -45,5 +45,95 @@ namespace FishScale.Repositorio
 
             return i >= 1;
         }
+
+        public List<Cliente> ObterClientes()
+        {
+            Connection();
+
+            List<Cliente> ClientesList = new List<Cliente>();
+
+            using (SqlCommand command = new SqlCommand("ObterClientes", conn1))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                conn1.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Cliente Clientes = new Cliente()
+                    {
+                        ClienteID = Convert.ToInt32(reader["ClienteID"]),
+                        NomeCliente = Convert.ToString(reader["NomeCliente"]),
+                        QtdeMaquinas = Convert.ToInt32(reader["QtdeMaquinas"]),
+                        AlertaHD = Convert.ToDouble(reader["AlertaHD"]),
+                        AlertaRam = Convert.ToDouble(reader["AlertaRAM"])
+                    };
+
+                    ClientesList.Add(Clientes);
+                }
+
+                conn1.Close();
+
+                return ClientesList;
+            }
+        }
+
+        public bool AtualizarCliente(Cliente clienteObj)
+        {
+
+            Connection();
+
+            int i;
+
+            using (SqlCommand command = new SqlCommand("AtualizarCliente", conn1))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ClienteID", clienteObj.ClienteID);
+                command.Parameters.AddWithValue("@QtdeMaquinas", clienteObj.QtdeMaquinas);
+                command.Parameters.AddWithValue("@NomeCliente", clienteObj.NomeCliente);
+                command.Parameters.AddWithValue("@AlertaHD", clienteObj.AlertaHD);
+                command.Parameters.AddWithValue("@AlertaRAM", clienteObj.AlertaRam);
+
+                conn1.Open();
+
+                i = command.ExecuteNonQuery();
+
+            }
+
+            conn1.Close();
+
+            return i >= 1;
+        }
+
+        public bool ExcluirCliente(int id)
+        {
+
+            Connection();
+
+            int i;
+
+            using (SqlCommand command = new SqlCommand("ExcluirClientePorId", conn1))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ClienteID", id);                              
+
+                conn1.Open();
+
+                i = command.ExecuteNonQuery();
+
+            }
+
+            conn1.Close();
+
+            if (i >= 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
     }
 }
