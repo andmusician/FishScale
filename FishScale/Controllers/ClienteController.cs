d5530705a7372a31f591fe1d428c805f7779e454
Anderson de Oliveira Lima <anderson.nlima@tivit.com>
2018-05-14 18:33:07 -0300

    Melhoria botão da página inicial

--- FishScale/Controllers/ClienteController.cs
+++ FishScale/Controllers/ClienteController.cs
@@ -1,9 +1,6 @@
 ﻿using FishScale.Models;
 using FishScale.Repositorio;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace FishScale.Controllers
