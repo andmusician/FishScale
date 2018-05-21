d2a31ec0d19a257eb033344ff92966d9d1558137
Aluno <Aluno@Digital.net>
2018-05-21 20:05:05 -0300

    Tirei os pontos (.) dos títulos das páginas

--- FishScale/Controllers/HomeController.cs
+++ FishScale/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace FishScale.Controllers
 
         public ActionResult Contato()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "";
 
             return View();
         }
