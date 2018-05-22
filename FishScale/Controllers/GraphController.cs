d5bb99c1e79d4daea31d129319a968f3fdeb2dab
Anderson de Oliveira Lima <anderson.nlima@tivit.com>
2018-05-22 17:54:31 -0300

    Criação da tela de gráficos integrados com Google Charts.

---
+++ FishScale/Controllers/GraphController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FishScale.Controllers
+{
+    [Authorize]
+    public class GraphController : Controller
+    {
+        // GET: Graph
+        public ActionResult Graficos()
+        {
+            return View();
+        }
+    }
+}
